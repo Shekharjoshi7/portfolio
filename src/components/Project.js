bcaf04c5df58a95750290b6393beab5bd7ab69fb
Shekhar <shekharjoshi813@gmail.com>
2023-06-18 19:00:24 +0530

    updating project

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,10 +2,26 @@ import React from 'react'
 import './Project.css'
 
 const Project = () => {
+  const weather = "img/Weather.JPG"
   return (
-    <div id='Project'>
+    <section id='Project'>
       <h1>Projects</h1>
-    </div>
+      <div className="CardBox">
+        <div className="cards">
+          <img src={weather} alt="" />
+          <div className="CardBtn">
+            <a href="https://shekharjoshi7.github.io/Weather-App/" target='_blank'>
+          <button className="btn btn-p">Live </button>
+            </a>
+
+            <a href="">
+          <button className="btn btn-p"> Code</button>
+            </a>
+          
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
