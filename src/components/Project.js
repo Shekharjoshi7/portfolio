246d1024354342e5aeb1e0ea3851fcc957715100
Shekhar <shekharjoshi813@gmail.com>
2023-06-18 19:50:40 +0530

    responsive project

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,24 +2,49 @@ import React from 'react'
 import './Project.css'
 
 const Project = () => {
+  const Data= [
+    {
+     src :"img/Weather.JPG",
+     Live: 'https://shekharjoshi7.github.io/Weather-App/',
+     Code:'https://github.com/Shekharjoshi7/Weather-App'},
+    {
+     src :"img/Our-Playlist.JPG",
+     Live: 'https://shekharjoshi7.github.io/Our-Playlist/',
+     Code:'https://github.com/Shekharjoshi7/Our-Playlist'},
+    {
+     src :"img/tic-tac-toe.JPG",
+     Live: 'https://shekharjoshi7.github.io/tic-tac-toe/',
+     Code:'https://github.com/Shekharjoshi7/tic-tac-toe'},
+    
+    ];
   const weather = "img/Weather.JPG"
   return (
     <section id='Project'>
       <h1>Projects</h1>
       <div className="CardBox">
-        <div className="cards">
-          <img src={weather} alt="" />
+
+        {Data.map((element,index)=>{
+        const {id,Live,Code,src} = element
+
+          return(<div className="cards">
+          <img src={src} alt="" />
+          <h2 className='m5'>Weather</h2>
           <div className="CardBtn">
-            <a href="https://shekharjoshi7.github.io/Weather-App/" target='_blank'>
+            <a className='m5' href= {Live} target='_blank'>
           <button className="btn btn-p">Live </button>
             </a>
 
-            <a href="">
-          <button className="btn btn-p"> Code</button>
+            <a className='m5' href={Code} target='_blank'>
+          <button className="btn btn-s"> Code</button>
             </a>
           
           </div>
-        </div>
+        </div>)
+
+        })
+        }
+        
+
       </div>
     </section>
   )
