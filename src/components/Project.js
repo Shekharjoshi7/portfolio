b2b33940a060a3accd0fd8caef74b7b22dc430c0
Shekhar <shekharjoshi813@gmail.com>
2023-08-26 20:51:57 +0530

    adding photo in profile

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,32 +4,37 @@ import './Project.css'
 const Project = () => {
   const Data= [
     {
+     title:'Weather',
      src :"img/Weather.JPG",
      Live: 'https://shekharjoshi7.github.io/Weather-App/',
      Code:'https://github.com/Shekharjoshi7/Weather-App'},
     {
+     title:'Our Playlist',
      src :"img/Our-Playlist.JPG",
      Live: 'https://shekharjoshi7.github.io/Our-Playlist/',
      Code:'https://github.com/Shekharjoshi7/Our-Playlist'},
     {
+     title:'Tic Tac Toe',
      src :"img/tic-tac-toe.JPG",
      Live: 'https://shekharjoshi7.github.io/tic-tac-toe/',
      Code:'https://github.com/Shekharjoshi7/tic-tac-toe'},
     
     ];
-  const weather = "img/Weather.JPG"
+  
   return (
     <section id='Project'>
       <h1>Projects</h1>
       <div className="CardBox">
 
         {Data.map((element,index)=>{
-        const {id,Live,Code,src} = element
-
-          return(<div className="cards">
+        const {title,Live,Code,src} = element
+ // eslint-disable-next-line 
+          return(
+          <div className="cards">
           <img src={src} alt="" />
-          <h2 className='m5'>Weather</h2>
+          <h2 className='m5'>{title}</h2>
           <div className="CardBtn">
+         
             <a className='m5' href= {Live} target='_blank'>
           <button className="btn btn-p">Live </button>
             </a>
@@ -39,12 +44,10 @@ const Project = () => {
             </a>
           
           </div>
-        </div>)
-
+        </div>
+        )
         })
         }
-        
-
       </div>
     </section>
   )
