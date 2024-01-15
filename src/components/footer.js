583e71092a0ce07b49bbe1efb3cba9cce5a46a5d
shekharjoshi <shekharjoshi813@gmail.com>
2024-01-15 16:07:24 +0530

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,33 @@ import './footer.css'
 const Footer = () => {
   return (
     <div id='footer'>
-      <p>All Rights Are Reserve</p>
+      <div className="FBox">
+        <div className='SBox'>
+          <div className="Fitem">
+            <h3>Get In Touch</h3>
+            <div className="Links">
+              <a href="https://www.instagram.com/shekhar__joshi/" className='Link'> <i className='fa-brands fa-instagram'></i>Instagram</a>
+              <a href="https://www.facebook.com/shekhar.joshi.739326" className='Link'><i className='fa-brands fa-facebook'></i>Facebook</a>
+              <a href="mailto:shekharjoshi813@gmail.com" className='Link'><i className='fa-solid fa-envelope'></i>Email</a>
+              <a href="" className='Link'><i className='fa-brands fa-twitter'></i>Twitter</a>
+            </div>
+          </div >
+          <div className="Fitem">
+            <h3>Quick Links</h3>
+            <div className="Links">
+              <a href="#About" className='Link'>About</a>
+              <a href="#Skills" className='Link'>Skills</a>
+              <a href="#Project" className='Link'>Project</a>
+              <a href="#Contact" className='Link'>Contact</a>
+            </div>
+          </div>
+        </div>
+        <div className="Top">
+          <a href="#Home">
+            <i class="fa-solid fa-arrow-up"></i>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
