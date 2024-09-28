da4c1205f45398ff9f0e82d04154282124ac93d5
Shekhar <shekharjoshi813@gmail.com>
2024-09-28 21:31:05 +0530

    .

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import './About.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css'
-
 const About = () => {
     const Certificate = './IMG/CERTIFICATE.JPG'
     const JavaScript = './IMG/JAVASCRIPT.PNG'
